2a3cf18a52fccf351ec923f5d201d7282f47f819
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-11-22 09:17:32 -0600

    Temporarily remove Chakra-UI provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Home />
-    </ChakraProvider>
+    <Home />
   </React.StrictMode>
 )
