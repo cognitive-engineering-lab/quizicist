cfc182f5a19049ddc3f5cf39b533948bb039c033
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-11-23 23:02:04 -0600

    Disable SWR revalidation on focus

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SWRConfig } from 'swr'
 import Home from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <SWRConfig value={{ revalidateOnFocus: false }}>
+      <Home />
+    </SWRConfig>
   </React.StrictMode>
 )
