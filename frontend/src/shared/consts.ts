389bebd1dc8797cdfe9ac67a2008369886d52b8f
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-11-29 17:44:34 -0600

    Add production server URL for frontend

--- frontend/src/shared/consts.ts
+++ frontend/src/shared/consts.ts
@@ -1,3 +1,5 @@
-export const SERVER_URL = "http://localhost:5000/api"; // TODO: change depending on node env
+export const SERVER_URL = process.env.NODE_ENV === "production" ?
+        "https://api.quizici.st/api" :
+        "http://localhost:5000/api";
 
 export const ALL_GENERATIONS_URL = `${SERVER_URL}/generated/all`;
