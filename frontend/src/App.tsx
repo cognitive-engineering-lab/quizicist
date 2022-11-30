6221a72867eaff29dd0466489afb022f6d125163
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-11-30 13:43:14 -0600

    Add message when no quizzes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, Divider } from "@chakra-ui/react";
+import { Container, Divider, Text } from "@chakra-ui/react";
 import GenerationView from "@components/GenerationView";
 import Upload from "@components/Upload";
 import useGenerations from "@hooks/useGenerations";
@@ -14,10 +14,13 @@ function Home() {
   return (
     <Container>
       <Upload />
+
       <Divider className={styles.divider} />
+
       {generations?.map((g) => (
         <GenerationView key={g} generation_id={g} />
       ))}
+      {generations?.length === 0 && <Text fontSize="large">You don't have any quizzes</Text>}
     </Container>
   );
 }
