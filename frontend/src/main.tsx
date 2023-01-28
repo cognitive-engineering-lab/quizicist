7d05c19f646c52a9fd4d9f8cf358ed9df7fe54fb
Connor Fogarty <connorflynnfogarty@gmail.com>
2023-01-28 16:05:24 -0600

    Add twitter metadata

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,14 +6,24 @@ import Navbar from "@wrappers/Navbar";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthWrapper>
         <BrowserRouter>
+          <Helmet>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta property="og:title" content="Quizicist: AI-powered quiz generator" />
+            <meta property="og:type" content="article" />
+            <meta property="og:description" content="Need to write a multiple-choice quiz? With your lecture notes or textbook pages, Quizicist can generate a quiz in minutes." />
+            <meta name="twitter:card" content="summary" />
+          </Helmet>
+
           <Navbar />
-          
+
           <Container maxW="container.lg" pt="2em">
             <Routes>
               <Route path="/" element={<Dashboard />} />
