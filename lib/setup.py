092b5f9da9d12a1f73de972803c83126cacd0152
Connor Fogarty <connorflynnfogarty@gmail.com>
2023-03-19 09:46:39 -0500

    Release `quizicist` pip package

--- lib/setup.py
+++ lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="quizicist",
-    version="0.1",
+    version="0.1.0",
     packages=["quizicist"],
     install_requires=[
         "openai==0.27.2", 
